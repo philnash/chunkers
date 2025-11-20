b43b098fbb2db1a787e9b1bf713060a778c82472
Phil Nash <philnash@gmail.com>
2025-11-21 08:57:40 +1100

    Redirect links to new blog post

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,8 +4,7 @@ export default function Footer() {
   return (
     <footer>
       <p>
-        Built by <a href="https://philna.sh/">philnash</a> for{" "}
-        <a href="https://www.datastax.com/devs?utm_medium=display&utm_source=datastax&utm_campaign=chunkers">DataStax</a>
+        Built by <a href="https://philna.sh/">philnash</a>
       </p>
     </footer>
   );
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,8 +4,8 @@ export default function Header() {
   return (
     <header>
       <h1>Chunkers</h1>
-      <p>An <a href="https://www.datastax.com/devs?utm_medium=display&utm_source=datastax&utm_campaign=chunkers&utm_content=">DataStax Devs</a> exploration of JavaScript text splitters.</p>
-      <p>Read about <a href="https://www.datastax.com/blog/how-to-chunk-text-in-javascript-for-rag-applications?utm_medium=display&utm_source=datastax&utm_campaign=chunkers">how to chunk text in JavaScript for your RAG application</a>.</p>
+      <p>An exploration of JavaScript text splitters.</p>
+      <p>Read about <a href="https://philna.sh/blog/2024/09/18/how-to-chunk-text-in-javascript-for-rag-applications/">how to chunk text in JavaScript for your RAG application</a>.</p>
     </header>
   );
 }
